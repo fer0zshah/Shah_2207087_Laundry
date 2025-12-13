7037c0456b577d476ffccaefc18bfad7c64ca9a3
Md Feroz Shah <shah2207087@stud.kuet.ac.bd>
2025-12-13 23:51:56 +0600

    initial commit

---
+++ src/main/java/com/example/landry/HelloController.java
@@ -0,0 +1,84 @@
+package com.example.landry;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class HelloController {
+
+    @FXML
+    private TextField userField;
+
+    @FXML
+    private PasswordField passField;
+
+    @FXML
+    private Label msglbl;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    public void Login(ActionEvent event) {
+        String user = userField.getText();
+        String pass = passField.getText();
+
+        if (user.equals("admin") && pass.equals("1234")) {
+            msglbl.setStyle("-fx-text-fill: green;");
+            msglbl.setText("Admin Login Success!");
+
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Admin.fxml"));
+                Scene scene = new Scene(fxmlLoader.load());
+                Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+                stage.setTitle("Admin Dashboard");
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        } else if (user.equals("client") && pass.equals("1234")) {
+            msglbl.setStyle("-fx-text-fill: blue;");
+            msglbl.setText("Customer Login Success!");
+
+
+        } else {
+            msglbl.setStyle("-fx-text-fill: red;");
+            msglbl.setText("Invalid Username or Password!");
+        }
+    }
+
+    @FXML
+    public void CreatAcc(ActionEvent event) {
+        try {
+            System.out.println("Navigating to Registration...");
+            msglbl.setStyle("-fx-text-fill: black;");
+            msglbl.setText("Loading Registration Screen...");
+
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Resister.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+
+            Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+
+            stage.setTitle("Create Account");
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            msglbl.setStyle("-fx-text-fill: red;");
+            msglbl.setText("Error: Could not load Resister.fxml");
+        }
+    }
+}
